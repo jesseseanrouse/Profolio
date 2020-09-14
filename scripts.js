85e9eb85a1c41e25823ffab565d777308f810d68
Jesse Rouse <jesserouse@apples-iMac-2.local>
2020-09-14 14:47:31 -0700

    Got a basic layout. Needs work

--- scripts.js
+++ scripts.js
@@ -2,16 +2,19 @@
 // https://spreadsheets.google.com/feeds/list/1Gy2RhQ_KJXX7U8FUJi-GXtvBNQlWTj140rriT0dlnDY/od6/public/values?alt=json
 
 
-
-const createArr = (projectsArr) => {
-    console.log(projectsArr)
-}
+// So I can see the array
+// const createArr = (projectsArr) => {
+//     console.log(projectsArr)
+// }
 
 const addProject2Index = (projectsArr) => {
 	let i = 0;
 	while (i < projectsArr.length) {
 		let title = projectsArr[i].title;
-		$('.projectList').append('<li>' + title) + '</li>';
+        $('.projectList').append('<li>' + title + '</li>');
+        let $img = $('<img>');
+        $img.attr('src', projectsArr[i].image)
+        $('.projectList').append($img);
 		i++;
 	}
 };
@@ -20,7 +23,7 @@ $.ajax({
 	url: "https://spreadsheets.google.com/feeds/list/1Gy2RhQ_KJXX7U8FUJi-GXtvBNQlWTj140rriT0dlnDY/od6/public/values?alt=json",
 }).then( sheet => {
         // so I can play find the path
-        console.log(sheet)
+        // console.log(sheet)
         // following the Joe's repo to do this
         const projects = sheet.feed.entry.map(projects =>{
             return {
@@ -31,7 +34,7 @@ $.ajax({
             }
         })
 
-        createArr(projects)
+        // createArr(projects)
         addProject2Index(projects);
 	}
 );
