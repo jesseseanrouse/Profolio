ef086d4059be23758e013e488946d510b259ca64
Jesse Rouse <jesserouse@apples-iMac-2.local>
2020-09-14 13:52:41 -0700

    I'm so EXCITED to get the project titles to list in javascript to the index html

--- scripts.js
+++ scripts.js
@@ -1,1 +1,37 @@
-let sheetAsJSON = https://spreadsheets.google.com/feeds/list/1Gy2RhQ_KJXX7U8FUJi-GXtvBNQlWTj140rriT0dlnDY/edit?usp=sharing/od6/public/values?alt=json
+// My Google spreadsheet link
+// https://spreadsheets.google.com/feeds/list/1Gy2RhQ_KJXX7U8FUJi-GXtvBNQlWTj140rriT0dlnDY/od6/public/values?alt=json
+
+
+
+const createArr = (projectsArr) => {
+    console.log(projectsArr)
+}
+
+const addProject2Index = (projectsArr) => {
+	let i = 0;
+	while (i < projectsArr.length) {
+		let title = projectsArr[i].title;
+		$('.projectList').append('<li>' + title) + '</li>';
+		i++;
+	}
+};
+
+$.ajax({
+	url: "https://spreadsheets.google.com/feeds/list/1Gy2RhQ_KJXX7U8FUJi-GXtvBNQlWTj140rriT0dlnDY/od6/public/values?alt=json",
+}).then( sheet => {
+        // so I can play find the path
+        console.log(sheet)
+        // following the Joe's repo to do this
+        const projects = sheet.feed.entry.map(projects =>{
+            return {
+                title: projects.gsx$title.$t,
+                link: projects.gsx$link.$t,
+                description: projects.gsx$description.$t,
+                image: projects.gsx$image.$t
+            }
+        })
+
+        createArr(projects)
+        addProject2Index(projects);
+	}
+);
