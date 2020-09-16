2b97162ac5fa07a6c02cf24b699f882f1db57bc8
Jesse Rouse <jesserouse@apples-iMac-2.local>
2020-09-16 12:06:20 -0700

    I got the link working on images

--- scripts.js
+++ scripts.js
@@ -36,17 +36,29 @@ const addProject2Index = (projectsArr) => {
         $('.projectList').append('<li>' + '</li>');
         let n = i + 1
 		let title = projectsArr[i].title;
-		$(`li:nth-of-type(${n})`).append('<header>' + title + '</header>');
-		let $img = $('<img>');
+        $(`li:nth-of-type(${n})`).append('<header>' + title + '</header>');
+        // Final: click on image and go to link
+        // Holy Code Batman I got it working
+        let link = projectsArr[i].link;
+        $(`li:nth-of-type(${n})`).append(`<a href="${link}"></a>`);
+        let $img = $('<img>');
 		$img.attr('src', projectsArr[i].image);
-        $(`li:nth-of-type(${n})`).append($img);
+        $(`li:nth-of-type(${n}) a`).append($img);
+        $(`li:nth-of-type(${n})`).append('<footer>This is a footer</footer>')
+        
+        // Image
+		// let $img = $('<img>');
+		// $img.attr('src', projectsArr[i].image);
+        // $(`li:nth-of-type(${n})`).append($img);
+
         //don't need this now or ever leaving for reference
 		// $($img).on('click', () => {
 		//     window.location = projectsArr[i].link
         // })
+
         // Got this working!!!!!!!!!!
-        let link = projectsArr[i].link;
-		$(`li:nth-of-type(${n})`).append(`<a href="${link}">Go to Site</a>`);
+        // let link = projectsArr[i].link;
+		// $(`li:nth-of-type(${n})`).append(`<a href="${link}">Go to Site</a>`);
 		i++;
     }
     caraStart();
